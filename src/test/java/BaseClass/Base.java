bc5a21777a8519a6f5fd6ff34d73c09a8b9d7f1a
Sonu <begumshajina4@gmail.com>
2025-09-06 20:01:05 +0530

    First Commit

--- src/test/java/BaseClass/Base.java
+++ src/test/java/BaseClass/Base.java
@@ -1,22 +1,51 @@
 package BaseClass;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Base {
-	public static WebDriver driver;
-
-	@BeforeTest
-	public void launch() {
-		//WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		driver.get("https://demoqa.com");
+		protected WebDriver driver;
+		protected String baseUrl;
 		
+		@BeforeClass
 		
+		public void setUp() {
+			WebDriverManager.chromedriver().setup();
+			
+			driver = new ChromeDriver();
+			baseUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+			
+			driver.get(baseUrl);//redirecting to the URL
+			//driver.navigate().to(baseUrl); //navigating to url
+			
+			driver.manage().window().maximize();// Maximize the tab/window
+			//driver.manage().window().minimize();// minimize the window
+			//driver.manage().window().fullscreen(); //full screen
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
-	}
-
-	
-
+			System.out.println("URL of the page: "+driver.getCurrentUrl());
+			System.out.println("Title: "+driver.getTitle());
+			//System.out.println("HTML Page source of page: "+driver.getPageSource());
+			//System.out.println("Window handles: "+driver.getWindowHandle());
+			
+			//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(80));
+			
+			
+		}
+		
+		 /*@AfterClass
+		    public void tearDown() {
+		        if (driver != null) {
+		            driver.quit();  // ✅ closes all browser windows and ends WebDriver session
+		            System.out.println("Browser closed successfully.");
+		        }
+		    }*/
+		
 }
